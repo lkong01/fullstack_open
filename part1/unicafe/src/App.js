eebcc5f68a9c0c9884ab644ede8aa93cedbaf42c
k <lingweikong10@gmail.com>
2022-11-14 16:02:44 -0600

    Finish q1.8, move statistics to its own component.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -14,6 +14,26 @@ const Statistic = ({ text, data }) => (
   </div>
 );
 
+const Statistics = ({ good, neutral, bad }) => {
+  const getTotal = () => good + neutral + bad;
+  const getAverage = () =>
+    good + neutral + bad ? (good - bad) / (good + neutral + bad) : 0; //avoid divisor is 0, show NaN
+  const getPositive = () =>
+    good + neutral + bad ? (good / (good + neutral + bad)) * 100 : 0;
+
+  return (
+    <div>
+      <Header text="statistics" />
+      <Statistic text="good" data={good} />
+      <Statistic text="neutral" data={neutral} />
+      <Statistic text="bad" data={bad} />
+      <Statistic text="all" data={getTotal()} />
+      <Statistic text="average" data={getAverage()} />
+      <Statistic text="positive" data={getPositive() + " %"} />
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -24,25 +44,13 @@ const App = () => {
   const increaseNeutral = () => setNeutral(neutral + 1);
   const increaseBad = () => setBad(bad + 1);
 
-  const getTotal = () => good + neutral + bad;
-  const getAverage = () =>
-    good + neutral + bad ? (good - bad) / (good + neutral + bad) : 0; //avoid divisor is 0, show NaN
-  const getPositive = () =>
-    good + neutral + bad ? (good / (good + neutral + bad)) * 100 : 0;
-
   return (
     <div>
       <Header text="give feedback" />
       <Button handleClick={increaseGood} option="good" />
       <Button handleClick={increaseNeutral} option="neutral" />
       <Button handleClick={increaseBad} option="bad" />
-      <Header text="statistics" />
-      <Statistic text="good" data={good} />
-      <Statistic text="neutral" data={neutral} />
-      <Statistic text="bad" data={bad} />
-      <Statistic text="all" data={getTotal()} />
-      <Statistic text="average" data={getAverage()} />
-      <Statistic text="positive" data={getPositive() + " %"} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
